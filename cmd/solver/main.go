eb25d5430995d89ecae5497c2c54b9f4404fb2c7
Benjamin Bengfort <benjamin@bengfort.com>
2026-01-07 09:46:23 -0600

    setup go pkg

---
+++ cmd/solver/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Placeholder for the main function of the solver command.
+	fmt.Println("solver")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module go.rtnl.ai/acme-linode
+
+go 1.25.1
+
+require go.rtnl.ai/x v1.9.0
